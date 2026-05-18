f8110d37517de4a01a00f86aa7eaba16aa12a6d5
Yurii <yurachellenuk@gmail.com>
2026-05-18 21:15:19 +0300

    feat: add DownloadQueue and worker for async downloads

---
+++ MediaDownloaderMVP/DownloadQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+using Telegram.Bot.Types;
+
+namespace MediaDownloaderTgBotMVP;
+
+public record DownloadTask(long ChatId, string Url, Message ProgressMessage);
+public class DownloadQueue
+{
+    private readonly Channel<DownloadTask> _channel =
+       Channel.CreateBounded<DownloadTask>(new BoundedChannelOptions(100)
+       {
+           FullMode = BoundedChannelFullMode.Wait
+       });
+
+    public ChannelWriter<DownloadTask> Writer => _channel.Writer;
+    public ChannelReader<DownloadTask> Reader => _channel.Reader;
+}
