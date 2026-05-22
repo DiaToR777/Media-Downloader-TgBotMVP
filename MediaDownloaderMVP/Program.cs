29863c061b42a1fdc8044582b80e920c30466523
Yurii <yurachellenuk@gmail.com>
2026-05-22 12:21:01 +0300

    feat: add caching

--- MediaDownloaderMVP/Program.cs
+++ MediaDownloaderMVP/Program.cs
@@ -10,32 +10,25 @@ var services = new ServiceCollection();
 var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
     ?? throw new Exception("CONNECTION_STRING missing");
 
-services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-services.AddSingleton<DownloadWorker>(provider =>
-{
-    var token = Environment.GetEnvironmentVariable("BOT_TOKEN")!;
-    var bot = new Telegram.Bot.TelegramBotClient(token);
-    var tempFolder = Path.Combine(AppContext.BaseDirectory, "downloads");
-    return new DownloadWorker(bot, tempFolder);
-});
-
 var token = Environment.GetEnvironmentVariable("BOT_TOKEN")
     ?? throw new Exception("BOT_TOKEN missing");
+
+services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(token));
 
 services.AddSingleton<DownloadWorker>(provider =>
 {
     var bot = provider.GetRequiredService<ITelegramBotClient>();
+    var scopeFactory = provider.GetRequiredService<IServiceScopeFactory>();
     var tempFolder = Path.Combine(AppContext.BaseDirectory, "downloads");
-    return new DownloadWorker(bot, tempFolder);
-}); 
+    return new DownloadWorker(bot, tempFolder, scopeFactory);
+});
 
 services.AddSingleton<TelegramService>();
-
 services.AddScoped<UserRepository>();
+services.AddScoped<CachedMediaRepository>();
 
 var provider = services.BuildServiceProvider();
 
@@ -47,5 +40,4 @@ using (var scope = provider.CreateScope())
 
 var telegramService = provider.GetRequiredService<TelegramService>();
 await telegramService.Start();
-
 await Task.Delay(-1);
